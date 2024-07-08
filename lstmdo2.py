1337da16b0866bb585c701d5c61e5592decae7a7
MarsChicken0508 <a0966827635@gmail.com>
2024-07-08 15:25:36 +0800

    push

--- lstmdo2.py
+++ lstmdo2.py
@@ -31,7 +31,7 @@ class lstm_model(nn.Module):
     def forward(self,x):
         x, _ = self.lstm(x)
         x = self.dropout(x)
-        #x = x[-1]   
+        x = x[:, -1, :]
         x = self.linear(x)
         
         return x
@@ -40,13 +40,14 @@ def start_train(model,loader,optimizer,question_data,ans_data,train_loss_array,t
     for epoch in range(n_epochs):
         model.train()
         for question_batch, ans_batch in loader:
+            question_batch = torch.randn(1, lookback, 2)
             ans_pred = model(question_batch)
             
-            #let_me_look = ans_pred.view(-1)
+            #let_me_look = ans_pred.view(0)
             #print(question_batch)
             #print("epoch=",epoch)
-            print(ans_pred)
-            loss = loss_fn(ans_pred,ans_batch[-1])
+            #print(ans_pred)
+            loss = loss_fn(ans_pred,ans_batch)
             '''
             ans_pred = 預測值
             ans_batch = 正確答案
@@ -60,10 +61,10 @@ def start_train(model,loader,optimizer,question_data,ans_data,train_loss_array,t
         model.eval()
         with torch.no_grad():
             ans_pred = model(question_data)
-            train_loss = loss_fn(ans_pred,question_data[-1]).item()
+            train_loss = loss_fn(ans_pred,question_data[:, -1, :]).item()
 
             ans_pred = model(ans_data)
-            test_loss = loss_fn(ans_pred,ans_data[-1]).item()
+            test_loss = loss_fn(ans_pred,ans_data[:, -1, :]).item()
             #print(epoch)
             train_loss_array.append(train_loss)
             test_loss_array.append(test_loss)
@@ -73,12 +74,12 @@ def start_train(model,loader,optimizer,question_data,ans_data,train_loss_array,t
     with torch.no_grad():
         model.eval()
 
-        train_plot = np.ones_like(input) * np.nan
+        train_plot = np.ones_like(question_data) * np.nan
         ans_pred = model(question_data)
-        train_plot[lookback:train_size] = model(question_data).view(-1)
+        train_plot[lookback:train_size] = model(question_data)
         test_plot = np.ones_like(input) * np.nan
-        test_plot[train_size + lookback : len(input)] = model(ans_data).view(-1)
-'''        train_loss_array.append(train_loss)
+        test_plot[train_size + lookback : len(input)] = model(ans_data).numpy()
+'''     train_loss_array.append(train_loss)
         test_loss_array.append(test_loss)'''
 #-----------------------------------------------main--------------------------------------------------------
 #讀檔
@@ -94,33 +95,26 @@ train_list,test_list = input[:train_size],input[train_size:]
 lookback = 10#一次抓取測試資料大小
 
 #切割好訓練集and測試集的數據以及各數據計算出的答案
-x_question_train,x_ans_train = create_dataset(train_list,lookback)
-y_question_train,y_ans_train = create_dataset(train_list,lookback)
-x_question_test,x_ans_test = create_dataset(test_list,lookback)
-y_question_test,y_ans_test = create_dataset(test_list,lookback)
+question_train,ans_train = create_dataset(train_list,lookback)
+question_test,ans_test = create_dataset(test_list,lookback)
 
-model_x = lstm_model()
-model_y = lstm_model()
-optimizer_x = optim.RMSprop(model_x.parameters())
-optimizer_y = optim.RMSprop(model_y.parameters())
+model = lstm_model()
+optimizer = optim.RMSprop(model.parameters())
+
 loss_fn = nn.HuberLoss()
-n_epochs = 200
+n_epochs = 101
 
-loader_x = data.DataLoader(data.TensorDataset(x_question_train,x_ans_train),shuffle=False,batch_size=lookback)
-loader_y = data.DataLoader(data.TensorDataset(y_question_train,y_ans_train),shuffle=False,batch_size=lookback)
+loader = data.DataLoader(data.TensorDataset(question_train,ans_train),shuffle=False,batch_size=lookback)
 train_loss_array = []
 test_loss_array = []
 
-start_train(model_x,loader_x,optimizer_x,x_question_train,x_question_test,train_loss_array,test_loss_array)
+start_train(model,loader,optimizer,question_train,question_test,train_loss_array,test_loss_array)
 
 train_loss_array = []
 test_loss_array = []
 
-start_train(model_y,loader_y,optimizer_y,y_question_train,y_question_test,train_loss_array,test_loss_array)
-
-x_ans = model_x(x_question_test)
-y_ans = model_y(y_question_test)
-opt_loader = data.DataLoader(data.TensorDataset(x_question_test,x_ans_test),shuffle=False,batch_size=lookback)
+ans = model(question_test)
+opt_loader = data.DataLoader(data.TensorDataset(question_test,ans_test),shuffle=False,batch_size=lookback)
 
 # 開啟輸出的 CSV 檔案
 with open('output.csv', 'w', newline='') as csvfile:# 建立 CSV 檔寫入器
@@ -128,13 +122,8 @@ with open('output.csv', 'w', newline='') as csvfile:# 建立 CSV 檔寫入器
     writer.writerow(['x', 'y'])
     for opt_array,ans_array in (opt_loader):
         #print(opt_array)
+        opt_array = torch.randn(1, lookback, 2)
         with torch.no_grad():
-            print(opt_array)
-            
-            x = model_x(opt_array).view(-1).detach().numpy()[0]
-            #x =model_x(opt_array).view(-1).detach().numpy().item()[0]
-            
-            y = model_y(opt_array).view(-1).detach().numpy()[0]
-        print(x)
-        print(y)
-        writer.writerow([x,y])
+            get_ans = model(opt_array)
+            print(get_ans)
+        writer.writerow([get_ans])
